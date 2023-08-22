a92b13921dabf85a3d1b4bfe1362a6a95ec15ccc
choang <charhoang@outlook.com>
2023-08-22 17:33:08 +0100

    fixed likes dislikes

--- forum/likes.go
+++ forum/likes.go
@@ -31,11 +31,12 @@ func getUserID(w http.ResponseWriter, r *http.Request) string {
 
 // Handler for handling like and dislike actions
 func HandleLikesDislikes(w http.ResponseWriter, r *http.Request) {
+	// Check session cookie
+	checkCookies(w, r)
+
 	// get postID, userID
 	postID = getPostID(w, r)
 	userID = getUserID(w, r)
-	fmt.Println(postID)
-	fmt.Println(userID)
 
 	r.ParseForm()
 	action := r.FormValue("action")
@@ -50,35 +51,17 @@ func HandleLikesDislikes(w http.ResponseWriter, r *http.Request) {
 	// Handle the like/dislike action based on user's previous interaction
 	// if r.Method == http.MethodPost {
 	if action == "like" {
-		// remove already existing like/dislike, add like
-		err := removeLikeDislike(userID, postID)
-		if err != nil {
-			http.Error(w, "Error removing like/dislike 1", http.StatusInternalServerError)
-			return
-		}
 		err = addLike(userID, postID)
 		if err != nil {
-			http.Error(w, "Error adding like 1", http.StatusInternalServerError)
+			http.Error(w, "Error adding like", http.StatusInternalServerError)
 			return
 		}
 	} else if action == "dislike" {
-		// remove already existing like/dislike, add dislikea
-		err := removeLikeDislike(userID, postID)
-		if err != nil {
-			http.Error(w, "Error removing like/dislike 2", http.StatusInternalServerError)
-			return
-		}
 		err = addDislike(userID, postID)
 		if err != nil {
-			http.Error(w, "Error adding dislike 1", http.StatusInternalServerError)
+			http.Error(w, "Error adding dislike", http.StatusInternalServerError)
 			return
 		}
-		// } else {
-		// 	err := addLike(userID, postID)
-		// 	if err != nil {
-		// 		http.Error(w, "Error adding like 2", http.StatusInternalServerError)
-		// 		return
-		// 	}
 	}
 
 	fmt.Println("Like/Dislike successful!")
@@ -91,8 +74,28 @@ func HandleLikesDislikes(w http.ResponseWriter, r *http.Request) {
 // Other database functions (e.g., addLike, removeLike, checkUserLikeDislike) should be implemented accordingly.
 
 // checkUserLikeDislike checks if a user has already liked or disliked a post.
+// func checkUserLikeDislike(userID string, postID int) (liked bool, disliked bool, err error) {
+// 	// Execute a query to check if the user has liked the post
+// 	row := DB.QueryRow("SELECT COUNT(*) FROM postlikes WHERE user_id = ? AND post_id = ? AND type = 1", userID, postID)
+// 	var likeCount int
+// 	if err := row.Scan(&likeCount); err != nil {
+// 		return false, false, err
+// 	}
+// 	liked = likeCount > 0
+
+// 	// Execute a query to check if the user has disliked the post
+// 	row = DB.QueryRow("SELECT COUNT(*) FROM postlikes WHERE user_id = ? AND post_id = ? AND type = -1", userID, postID)
+// 	var dislikeCount int
+// 	if err := row.Scan(&dislikeCount); err != nil {
+// 		return false, false, err
+// 	}
+// 	disliked = dislikeCount > 0
+
+// 	return liked, disliked, nil
+// }
+
 func checkUserLikeDislike(userID string, postID int) (liked bool, disliked bool, err error) {
-	// Execute a query to check if the user has liked the post
+	// Execute a query to check if the user has liked or disliked the post
 	row := DB.QueryRow("SELECT COUNT(*) FROM postlikes WHERE user_id = ? AND post_id = ? AND type = 1", userID, postID)
 	var likeCount int
 	if err := row.Scan(&likeCount); err != nil {
@@ -100,7 +103,6 @@ func checkUserLikeDislike(userID string, postID int) (liked bool, disliked bool,
 	}
 	liked = likeCount > 0
 
-	// Execute a query to check if the user has disliked the post
 	row = DB.QueryRow("SELECT COUNT(*) FROM postlikes WHERE user_id = ? AND post_id = ? AND type = -1", userID, postID)
 	var dislikeCount int
 	if err := row.Scan(&dislikeCount); err != nil {
@@ -108,61 +110,80 @@ func checkUserLikeDislike(userID string, postID int) (liked bool, disliked bool,
 	}
 	disliked = dislikeCount > 0
 
+	// check for data entries that have not been liked or disliked
+	row = DB.QueryRow("SELECT COUNT(*) FROM postlikes WHERE user_id = ? AND post_id = ? AND type = 0", userID, postID)
+	var neitherCount int
+	if err := row.Scan(&neitherCount); err != nil {
+		return false, false, err
+	}
+
+	// If there is a "neither" entry, treat it as a like and a dislike
+	if neitherCount > 0 {
+		liked = true
+		disliked = true
+	}
+
 	return liked, disliked, nil
 }
 
 func addLike(userID string, postID int) error {
-	_, err := DB.Exec("INSERT INTO postlikes (user_id, post_id, type) VALUES (?, ?, '1')", userID, postID)
+	existingLike, existingDislike, err := checkUserLikeDislike(userID, postID)
 	if err != nil {
 		return err
+	}
+
+	if existingDislike {
+		// Replace value '-1' with '1'
+		_, err = DB.Exec("UPDATE postlikes SET type = 1 WHERE user_id = ? AND post_id = ?", userID, postID)
+		if err != nil {
+			return err
+		}
+		fmt.Println("Replaced Dislike with Like")
+	} else if existingLike {
+		// Toggle value '1' to '0'
+		_, err = DB.Exec("UPDATE postlikes SET type = 0 WHERE user_id = ? AND post_id = ?", userID, postID)
+		if err != nil {
+			return err
+		}
+		fmt.Println("Toggled Like to Neither")
+	} else {
+		// Insert new entry with value '1'
+		_, err = DB.Exec("INSERT INTO postlikes (user_id, post_id, type) VALUES (?, ?, 1)", userID, postID)
+		if err != nil {
+			return err
+		}
+		fmt.Println("Added Like")
 	}
 	return nil
 }
 
 func addDislike(userID string, postID int) error {
-	_, err := DB.Exec("INSERT INTO postlikes (user_id, post_id, type) VALUES (?, ?, '-1')", userID, postID)
+	existingLike, existingDislike, err := checkUserLikeDislike(userID, postID)
 	if err != nil {
 		return err
 	}
-	return nil
-}
 
-func removeLikeDislike(userID string, postID int) error {
-	// _, err := DB.Exec("INSERT INTO postlikes (user_id, post_id, type) VALUES (?, ?, 0)", userID, postID)
-	_, err := DB.Exec("DELETE FROM postlikes WHERE user_id = ? AND post_id = ?", userID, postID)
-	if err != nil {
-		return err
+	if existingLike {
+		// Replace value '1' with '-1'
+		_, err = DB.Exec("UPDATE postlikes SET type = -1 WHERE user_id = ? AND post_id = ?", userID, postID)
+		if err != nil {
+			return err
+		}
+		fmt.Println("Replaced Like with Dislike")
+	} else if existingDislike {
+		// Toggle value '-1' to '0'
+		_, err = DB.Exec("UPDATE postlikes SET type = 0 WHERE user_id = ? AND post_id = ?", userID, postID)
+		if err != nil {
+			return err
+		}
+		fmt.Println("Toggled Dislike to Neither")
+	} else {
+		// Insert new entry with value '-1'
+		_, err = DB.Exec("INSERT INTO postlikes (user_id, post_id, type) VALUES (?, ?, -1)", userID, postID)
+		if err != nil {
+			return err
+		}
+		fmt.Println("Added Dislike")
 	}
-	fmt.Println("Like/Dislike removed successfully")
 	return nil
 }
-
-// func PostLikesHandler(w http.ResponseWriter, r *http.Request) {
-// 	// Check session cookie
-// 	checkCookies(w, r)
-
-// 	// Get postID from URL path
-// 	postIDStr := strings.TrimPrefix(r.URL.Path, "/post-like/")
-// 	postID, err := strconv.Atoi(postIDStr)
-// 	if err != nil {
-// 		http.Error(w, "Invalid post ID", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	// Retrieve the sessionID and userID from the cookie
-// 	_, userId, err := CommentgetCookieValue(r)
-// 	if err != nil {
-// 		http.Error(w, "cookie not found", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	// insert data into table
-// 	_, err = DB.Exec("INSERT INTO postlikes (user_id, post_id) VALUES (?, ?)",
-// 		userId, postID)
-// 	if err != nil {
-// 		log.Println(err)
-// 		http.Error(w, "Could not insert data to table", http.StatusInternalServerError)
-// 		return
-// 	}
-
-// }
